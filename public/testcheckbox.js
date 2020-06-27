266b2d70de8990aab5305cfcaac17c4abfca5bd4
Akarsh Ojha <akarsho@users.noreply.github.com>
2020-06-27 16:09:29 -0700

    FIXED CHECKLIST WHKJLELHKSDKLHS

--- public/testcheckbox.js
+++ public/testcheckbox.js
@@ -1,31 +1,26 @@
+let string1 = "A";
+let string2 = "B";
+let string3 = "C";
 
-let youtubers = [
-    {
-        name: "markipliers",
-        state: false
-    },
-    {
-        name: "jack",
-        state: false
-    },
-    {
-        name: "pewdie",
-        state: false
-    }
-]
+let ytbers = [string1, string2, string3];
 
-console.log(youtubers);
-let placeholder = document.getElementById("feedFilter");
-for(let item of youtubers){
-    let temp = document.createElement("div");
-    temp.className = "checklist"
-    let html = `
-    <label>
-        <input type="checkbox" checked>${item.name}
-    </label>
-
-    `
-    temp.innerHTML = html;
-    placeholder.appendChild(temp);
-    
+console.log(ytbers);
+for(let item of ytbers){
+    let bigdiv = document.getElementById("bigdiv");
+    bigdiv.setAttribute("style","display: inline-block");
+    let placeholder = document.createElement("div")
+    placeholder.setAttribute("style","display: inline-flex;")
+    let x = document.createElement("input");
+    x.type = "checkbox";
+    x.name = "name";
+    x.value = "value";
+    x.id =  "id";
+    let label = document.createElement('label');
+    label.htmlFor = "id";
+    label.append(document.createTextNode(item));
+    placeholder.appendChild(x);
+    placeholder.appendChild(label);
+    bigdiv.appendChild(placeholder);
+    let brk = document.createElement("br");
+    bigdiv.appendChild(brk);
 }
