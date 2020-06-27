c9768162d7980b724c9b4788952f68f5a3c2afaf
jayp-png <60831681+jayp-png@users.noreply.github.com>
2020-06-27 11:32:02 -0700

    sfdh

--- public/testcheckbox.js
+++ public/testcheckbox.js
@@ -1,21 +1,16 @@
 let string1 = "A";
 let string2 = "B";
 let string3 = "C"
-let ytbers = new Array(string1, string2, string3);
-let arlen = ytbers.length;
-
-for(let i = 0; i < arlen; i++ ){
-    let placeholder = document.getElementById("xyz");
-    let x = document.createElement("input");
-    x.type = "checkbox";
-    x.name = "name";
-    x.value = "value";
-    x.id = "id";
-    let label = document.createElement('label');
-    label.htmlFor = "id"
-    label.append(document.createTextNode(ytbers[i].toString()));
-    placeholder.appendChild(x);
-    placeholder.appendChild(label);
-    let brk = document.createElement("br");
-    placeholder.appendChild(brk);
+let ytbers = [string1, string2, string3];
+console.log(ytbers);
+let placeholder = document.getElementById("xyz");
+for(let item of ytbers){
+    let temp = document.createElement("div");
+    let html = `
+        <label> ${item}
+            <input type="checkbox" checked>
+        </label>
+    `
+    temp.innerHTML = html;
+    placeholder.appendChild(temp);
 }
