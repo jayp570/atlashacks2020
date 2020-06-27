b1008ed9166ede1c51195568842e1cfe2a0b9e93
adrian154 <94568a@gmail.com>
2020-06-27 09:47:03 -0700

    working

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+let searchResultElems = [];
+
 const go = function() {
 
     let channelName = document.getElementById("field-channelName").value;
@@ -5,23 +7,37 @@ const go = function() {
     searchForChannel(channelName).then(
         (result) => {
             try {
-                
+
                 let response = JSON.parse(result);
+                console.log(response);
                 document.getElementById("searchGlobalText").innerHTML = `${response.pageInfo.totalResults} results for "${channelName}"`;
                 
+                // Destroy search results
+                for(let elem of searchResultElems) {
+                    elem.remove();
+                }
+
                 let searchResultsArea = document.getElementById("searchResults");
                 for(let item of response.items) {
                     
                     let subDiv = document.createElement("div");
                     searchResultsArea.appendChild(subDiv);
+                    searchResultElems.push(subDiv);
+
+                    let channelNameElem = document.createElement("p");
+                    channelNameElem.appendChild(document.createTextNode(item.snippet.channelTitle));
+                    subDiv.appendChild(channelNameElem);
+
+                    let channelThumbnailElem = document.createElement("img");
+                    channelThumbnailElem.src = item.snippet.thumbnails.default.url;
+                    subDiv.appendChild(channelThumbnailElem);
 
                 }
-
-                console.log(response);
 
             } catch(error) {
                 console.log("Bad response from the server: ", error);
             }
+        
         },
         (error) => {
             console.log("Error: ", error);
