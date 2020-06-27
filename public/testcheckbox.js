a86fec0cb3ae486ce94f1fef932b3853ed3e17d0
jayp-png <60831681+jayp-png@users.noreply.github.com>
2020-06-27 11:04:21 -0700

    Update testcheckbox.js

--- public/testcheckbox.js
+++ public/testcheckbox.js
@@ -1,16 +1,19 @@
 let string1 = "A";
 let string2 = "B";
 let string3 = "C"
-let ytbers = [string1, string2, string3];
-console.log(ytbers);
-for(let item of ytbers){
+let ytbers = new Array(string1, string2, string3);
+let arlen = ytbers.length;
+
+for(let i = 0; i < arlen; i++ ){
     let placeholder = document.getElementById("xyz");
     let x = document.createElement("input");
     x.type = "checkbox";
+    x.name = "name";
+    x.value = "value";
     x.id = "id";
     let label = document.createElement('label');
     label.htmlFor = "id"
-    label.append(document.createTextNode(item));
+    label.append(document.createTextNode(ytbers[i].toString()));
     placeholder.appendChild(x);
     placeholder.appendChild(label);
     let brk = document.createElement("br");
