a08e2a2db75832272322970245ac4900493a028c
Akarsh Ojha <akarsho@users.noreply.github.com>
2020-06-27 10:26:48 -0700

    checkboxe

---
+++ public/testcheckbox.js
@@ -0,0 +1,21 @@
+let string1 = "A";
+let string2 = "B";
+let string3 = "C"
+let ytbers = new Array(string1, string2, string3);
+let arlen = ytbers.length;
+
+for(let i = 0; i < arlen; i++ ){
+    let placeholder = document.getElementById("xyz");
+    let x = document.createElement("input");
+    x.type = "checkbox";
+    x.name = "name";
+    x.value = "value";
+    x.id = "id";
+    let label = document.createElement('label');
+    label.htmlFor = "id"
+    label.append(document.createTextNode(ytbers[i].toString()));
+    placeholder.appendChild(x);
+    placeholder.appendChild(label);
+    let brk = document.createElement("br");
+    placeholder.appendChild(brk);
+}
