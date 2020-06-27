2ff90ace92c5992f2da3ef6de328082f20a8f511
jayp-png <60831681+jayp-png@users.noreply.github.com>
2020-06-27 12:14:02 -0700

    homei

--- public/testcheckbox.js
+++ public/testcheckbox.js
@@ -1,16 +1,45 @@
 let string1 = "A";
 let string2 = "B";
 let string3 = "C"
-let ytbers = [string1, string2, string3];
-console.log(ytbers);
+let youtubers = [
+    {
+        name: string1,
+        state: false
+    },
+    {
+        name: string2,
+        state: false
+    },
+    {
+        name: string3,
+        state: false
+    }
+]
+
+function toggleButton(creator) {
+    for(let item of youtubers) {
+        if(item.name == creator) {
+            item.state = !item.state;
+        }
+        document.getElementById(creator+"Button").style.backgroundColor = "#111";
+        document.getElementById(creator+"Button").style.color = "white";
+        if(item.state) {
+            document.getElementById(creator+"Button").style.backgroundColor = "white";
+            document.getElementById(creator+"Button").style.color = "#111";
+        }
+    }
+}
+
+
+console.log(youtubers);
 let placeholder = document.getElementById("xyz");
-for(let item of ytbers){
+for(let item of youtubers){
     let temp = document.createElement("div");
     let html = `
-        <label> ${item}
-            <input type="checkbox" checked>
-        </label>
+    <button onclick="toggleButton('${item.name}')" class="filterButton" id="${item.name}Button">${item.name}</button>
+
     `
     temp.innerHTML = html;
     placeholder.appendChild(temp);
+    
 }
