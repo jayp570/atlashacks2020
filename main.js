8d2848939016278f11b67aabf0f3452e28948ce7
adrian154 <94568a@gmail.com>
2020-06-27 18:42:38 -0700

    apis

--- main.js
+++ main.js
@@ -4,10 +4,20 @@
 const express = require("express");
 const requester = require("request");
 const auth = require("./auth_module.js");
+const Twitter = require("twitter");
+const auth_module = require("./auth_module.js");
 
 // App variables
 const app = express();
 const PORT = 80;
+
+// Init twitter
+const twitter = new Twitter({
+    consumer_key: auth_module.TWITTER_API_KEY,
+    consumer_secret: auth_module.TWITTER_API_KEY_SECRET,
+    access_token_key: auth_module.TWITTER_ACCESS_TOKEN,
+    access_token_secret: auth_module.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 // Routes
 // Serve static pages from public dir
@@ -39,6 +49,20 @@ app.get("/api/cors-passthrough", (request, response) => {
 
     } catch(error) {
         console.log("Problem while handling CORS passthrough request: ", error);
+    }
+});
+
+app.get("/api/test", (request, response) => {
+    try {
+        twitter.get("statuses/user_timeline", {screen_name: request.query.name}, (error, tweet, response2) => {
+            if(!error) {
+                response.send(response2);
+            } else {
+                response.status(400).json({type: "error"})   
+            }
+        });
+    } catch(error) {
+        console.log("Problem while handling Twitter request: ", error);
     }
 });
 
