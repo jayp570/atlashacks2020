65db39913133f511dca1afa3e3f217b5e9a83932
jayp-png <60831681+jayp-png@users.noreply.github.com>
2020-06-28 18:32:27 -0700

    added platform filtering

--- public/main.js
+++ public/main.js
@@ -119,13 +119,13 @@ const addYTSearchResult = async function(item) {
             // console.log(ytbers);
 
             if(document.getElementById("twtbtn").checked == true) {
-                showingTwitter = false;
+                showingTwitter = true;
             } else {    
                 showingTwitter = false
             }
 
             if(document.getElementById("ytbtn").checked == true) {
-                showingYoutube = false;
+                showingYoutube = true;
             } else {    
                 showingYoutube = false
             }
